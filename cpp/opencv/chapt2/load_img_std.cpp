9330da7bad8292f80b8c16805db1b87dd499cc82
William Goble <will.goble@gmail.com>
2021-12-22 14:13:29 -0500

    adds error handling for second example.

--- cpp/opencv/chapt2/load_img_std.cpp
+++ cpp/opencv/chapt2/load_img_std.cpp
@@ -1,9 +1,17 @@
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
 int main(int argc, char** argv)
 {
+	if(argc != 2)
+	{
+		cout << "Usage: load_img_std.x img_to_display\n";
+		return -1;
+	}
+
 	Mat img = imread(argv[1], -1);
 
 	if(img.empty())
